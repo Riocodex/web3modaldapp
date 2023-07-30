2aa430b20f39d7673a3c039e5ae5cf7f6828d4d3
Riocodex <onwukachibike@gmail.com>
2023-07-30 05:23:08 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import './form.css';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal, Web3Button, useWeb3ModalTheme } from '@web3modal/react'
+import { useState } from 'react';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { sepolia } from 'wagmi/chains'
+
 
 function App() {
+  const chains = [sepolia]
+  const projectId = '8ab1f09f61d98b698626395d47355c8e'
+  const { theme, setTheme } = useWeb3ModalTheme()
+  // State to store the value entered in the input
+  const [inputValue, setInputValue] = useState('');
+
+  // Event handler to update the state when input changes
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  setTheme({themeColor:"orange"})
+  
+  const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors: w3mConnectors({ projectId, chains }),
+    publicClient
+  })
+  const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <WagmiConfig config={wagmiConfig}>
+        <h1>Wallet Connect Dapp</h1>
+        <Web3Button />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+
+       <div className="form-container">
+      <form className="form">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter address"
+        className="form-input"
+      />
+        <button type="submit" className="form-button">Submit</button>
+      </form>
+    </div>
+    
+
     </div>
   );
 }
