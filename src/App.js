c67919546bb0c602313a217da98c1f8d04a70e49
Riocodex <onwukachibike@gmail.com>
2023-07-30 18:36:25 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -3,11 +3,212 @@ import './form.css';
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal, Web3Button, useWeb3ModalTheme } from '@web3modal/react'
 import { useState } from 'react';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { configureChains, createConfig, WagmiConfig} from 'wagmi'
+import { useContractRead } from 'wagmi'
 import { sepolia } from 'wagmi/chains'
 
 
 function App() {
+  const faucetAbi = {
+    "abi": [
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "tokenAddress",
+            "type": "address"
+          }
+        ],
+        "stateMutability": "payable",
+        "type": "constructor"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "from",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "Deposit",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "by",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "EthWithdrawn",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "from",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "FeePaid",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "Withdrawal",
+        "type": "event"
+      },
+      {
+        "inputs": [],
+        "name": "getBalance",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "lockTime",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "recipient",
+            "type": "address"
+          }
+        ],
+        "name": "requestTokens",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "setLockTime",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "setWithdrawalAmount",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "token",
+        "outputs": [
+          {
+            "internalType": "contract IERC20",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "withdraw",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "withdrawEth",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "withdrawalAmount",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "stateMutability": "payable",
+        "type": "receive"
+      }
+    ],
+  }
   const chains = [sepolia]
   const projectId = '8ab1f09f61d98b698626395d47355c8e'
   const { theme, setTheme } = useWeb3ModalTheme()
@@ -19,6 +220,10 @@ function App() {
     setInputValue(event.target.value);
   };
 
+  const handleButton = async()=>{
+    console.log('clicked')
+  }
+
   setTheme({themeColor:"orange"})
   
   const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
@@ -26,6 +231,12 @@ function App() {
     autoConnect: true,
     connectors: w3mConnectors({ projectId, chains }),
     publicClient
+  })
+
+  const { data, isError, isLoading } = useContractRead({
+    address: '0xDDB2DD250821552A10B4a8E303f8bbE16aDF2104',
+    abi: faucetAbi.abi,
+    functionName: 'getBalance',
   })
   const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
@@ -41,7 +252,7 @@ function App() {
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
 
        <div className="form-container">
-      <form className="form">
+      <div className="form">
       <input
         type="text"
         value={inputValue}
@@ -49,9 +260,14 @@ function App() {
         placeholder="Enter address"
         className="form-input"
       />
-        <button type="submit" className="form-button">Submit</button>
-      </form>
+        <button 
+        className="form-button"
+        onClick={handleButton}
+        >Submit</button>
+      </div>
     </div>
+
+    <p>Balance = {data}</p>
     
 
     </div>
